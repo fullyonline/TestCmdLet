797853504276b2c28d9692714b455a62e1a1e85b
fullyonline <no.reply4me@gmail.com>
2022-10-30 10:43:08 +0100

    Projektdateien hinzufügen.

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace TestCmdLet
+{
+    [Cmdlet(VerbsCommon.Get, "Greeting")]
+    public class HelloWorld: PSCmdlet
+    {
+        [Parameter(ValueFromPipeline =true)]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var whoDat = string.IsNullOrEmpty(Name) ? "World" : Name;
+            var greeting = "Hello " + whoDat;
+
+            WriteObject(greeting);
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,26 @@
+﻿# Installation
+
+all commands are executet on the cmd
+
+## new classlib
+
+```
+dotnet new classlib –name <Name>
+cd <Name>
+```
+
+## add PowerShell capabilities
+
+```
+dotnet add package PowerShellStandard.Library
+```
+
+## or create it from a template:
+
+```
+dotnet new -i Microsotft.PowerShell.Standard.Module.Template
+```
+
+```
+dotnet new psmodule
+```
