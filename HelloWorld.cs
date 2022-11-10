6b5e16d99c2dc4831c3e8376483662908d745294
fullyonline <no.reply4me@gmail.com>
2022-11-10 16:26:42 +0100

    added all methods

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -9,12 +9,27 @@ namespace TestCmdLet
         [Parameter(ValueFromPipeline =true)]
         public string Name { get; set; }
 
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
         protected override void ProcessRecord()
         {
             var whoDat = string.IsNullOrEmpty(Name) ? "World" : Name;
             var greeting = "Hello " + whoDat;
 
             WriteObject(greeting);
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
         }
     }
 }
